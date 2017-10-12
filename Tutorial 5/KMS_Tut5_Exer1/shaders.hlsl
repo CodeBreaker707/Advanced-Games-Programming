eb3d94f7efd037bef9cd509f096b3c905ab6d5fa
CodeBreaker707 <2solak01@solent.ac.uk>
2017-10-12 18:01:06 +0100

    Tut 5 and Tut 6

---
+++ Tutorial 5/KMS_Tut5_Exer1/shaders.hlsl
@@ -0,0 +1,30 @@
+cbuffer Cbuffer0
+{
+	matrix WVPMatrix; // 64 bytes
+	float red_fraction; // 4 bytes
+	float scale; // 4 bytes
+	float2 packing; // 2x4 bytes = 8 bytes
+};
+
+struct VOut
+{
+	float4 position : SV_POSITION;
+	float4 color : COLOR;
+};
+
+VOut VShader(float4 position : POSITION, float4 color : COLOR)
+{
+	VOut output;
+
+	color.rb *= red_fraction;
+	//position.xy *= scale;
+	output.position = mul(WVPMatrix, position);
+	output.color = color;
+
+	return output;
+}
+
+float4 PShader(float4 position : SV_POSITION, float4 color : COLOR) : SV_TARGET
+{
+	return color;
+}
