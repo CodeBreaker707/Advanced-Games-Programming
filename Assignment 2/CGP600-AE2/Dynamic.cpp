b271d4dc380b1f77cf0bc3f3a1c9a621c048ca9d
CodeBreaker707 <2solak01@solent.ac.uk>
2017-12-20 18:37:23 +0300

    Commented Dynamic.h

--- Assignment 2/CGP600-AE2/Dynamic.cpp
+++ Assignment 2/CGP600-AE2/Dynamic.cpp
@@ -2,11 +2,8 @@
 
 Dynamic::Dynamic(ID3D11Device* D3DDevice, ID3D11DeviceContext* ImmediateContext, char* assetFile, char* textureFile, float x_scale, float y_scale, float z_scale)
 {
+	// Initialising the dynamic asset
 	InitialiseAsset(D3DDevice, ImmediateContext, assetFile, textureFile, x_scale, y_scale, z_scale);
-
-	//LoadObjModel("Assets/cube2.obj", "Assets/tile.bmp");
-
-	m_isPushing = false;
 }
 
 Dynamic::~Dynamic()
--- Assignment 2/CGP600-AE2/Dynamic.h
+++ Assignment 2/CGP600-AE2/Dynamic.h
@@ -1,19 +1,15 @@
 #pragma once
 
+// Including the Asset class
+// for loading in the model and to draw it
 #include "Asset.h"
 
 class Dynamic : public Asset
 {
-private:
-	
-	bool m_isPushing;
-
 public:
 
+	// Class Constructor and Destructor
 	Dynamic(ID3D11Device* D3DDevice, ID3D11DeviceContext* ImmediateContext, char* assetFile, char* textureFile, float x_scale, float y_scale, float z_scale);
 	~Dynamic();
-
-	//void SetPushState(bool state);
-	//bool GetPushState();
 
 };
