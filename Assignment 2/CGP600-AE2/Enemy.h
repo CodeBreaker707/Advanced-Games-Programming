3c47fc7d16fba48a8ed265b12cb190a944a9869c
CodeBreaker707 <2solak01@solent.ac.uk>
2017-12-20 18:44:20 +0300

    Commented Enemy.h

--- Assignment 2/CGP600-AE2/Enemy.h
+++ Assignment 2/CGP600-AE2/Enemy.h
@@ -1,17 +1,22 @@
 #pragma once
 
+// Including the asset class to load
+// and draw the model
 #include "Asset.h"
 
 class Enemy : public Asset
 {
 private:
 
+	// This stores the enemy's health
 	float m_health;
 
 public:
+	// Class Constructor and Destructor
 	Enemy(ID3D11Device* D3DDevice, ID3D11DeviceContext* ImmediateContext, char* assetFile, char* textureFile, float x_scale, float y_scale, float z_scale);
 	~Enemy();
 
+	// Set and Get functions for enemy's health
 	void SetEnemyHealth(float amount);
 	float GetEnemyHealth();
 
