76fe9f8817e3242a75cd35dc32997700f5fab6a6
CodeBreaker707 <2solak01@solent.ac.uk>
2017-12-22 10:56:27 +0300

    Commented Light.h

--- Assignment 2/CGP600-AE2/Light.h
+++ Assignment 2/CGP600-AE2/Light.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// Definitions for XNAMATH to
+// work efficiently
 #define _XM_NO_INTRINSICS_
 #define XM_NO_ALIGNMENT
 
@@ -11,19 +13,29 @@ class Light
 {
 private:
 
+	// Vector to project the light from a position
 	XMVECTOR m_directional_light_shines_from;
+
+	// Vector to determine the colour of the light
 	XMVECTOR m_directional_light_colour;
+
+	// Vector to determine the ambient
+	// light colour. If there is no light,
+	// this will illuminate the world
 	XMVECTOR m_ambient_light_colour;
 
 public:
 
+	// Constructor and Destructor
 	Light();
 	~Light();
 
+	// Set functions for the vectors declared above
 	void SetDirectionalLightPos(float x, float y, float z, float w);
 	void SetDirectionalLightColour(float r, float g, float b, float a);
 	void SetAmbientLightColour(float r, float g, float b, float a);
 
+	// Get functions for the vectors declared above
 	XMVECTOR GetDirectionalLightPos();
 	XMVECTOR GetDirectionalLightColour();
 	XMVECTOR GetAmbientLightColour();
