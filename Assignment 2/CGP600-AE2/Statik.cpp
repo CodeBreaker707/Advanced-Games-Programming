7ad0025de36d13075a2fe1f8fa66516c80050bbf
CodeBreaker707 <2solak01@solent.ac.uk>
2017-12-25 12:45:19 +0300

    Commented Statik.cpp

--- Assignment 2/CGP600-AE2/Statik.cpp
+++ Assignment 2/CGP600-AE2/Statik.cpp
@@ -1,11 +1,14 @@
 #include "Statik.h"
 
+// Constructor
 Statik::Statik(ID3D11Device* D3DDevice, ID3D11DeviceContext* ImmediateContext, char* assetFile, char* textureFile, float x_scale, float y_scale, float z_scale)
 {
+	// Initialising the asset
 	InitialiseAsset(D3DDevice, ImmediateContext, assetFile, textureFile, x_scale, y_scale, z_scale);
 
 }
 
+// Destructor
 Statik::~Statik()
 {
 
--- Assignment 2/CGP600-AE2/Weapon.cpp
+++ Assignment 2/CGP600-AE2/Weapon.cpp
@@ -1,5 +1,6 @@
 #include "Weapon.h"
 
+// Constructor
 Weapon::Weapon(ID3D11Device* D3DDevice, ID3D11DeviceContext* ImmediateContext, char* assetFile, char* textureFile, float x_scale, float y_scale, float z_scale)
 {
 	// Initialising the asset
