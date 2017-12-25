dca004baccc9030826a6761668b63fbdd18723d3
CodeBreaker707 <2solak01@solent.ac.uk>
2017-12-25 10:41:02 +0300

    Finished commenting SceneNode.h

--- Assignment 2/CGP600-AE2/SceneNode.h
+++ Assignment 2/CGP600-AE2/SceneNode.h
@@ -117,109 +117,176 @@ private:
 	bool m_in_range;
 
 	// This is to halt the movement of the entity
-	// if it approached the player
+	// if it had approached the player
 	bool m_halt_movement;
 
 public:
 
+	// Declaring Objects
 	Player* m_p_asset;
 	Enemy*  m_e_asset;
 	Weapon* m_w_asset;
 	Statik* m_s_asset;
 	Dynamic* m_d_asset;
 
+	// Constructor and Destructor
 	SceneNode(ID3D11Device* D3DDevice, ID3D11DeviceContext* ImmediateContext, char c, char* assetFile, char* textureFile, float x_pos, float y_pos, float z_pos, float x_scale, float y_scale, float z_scale, int gravityState);
 	~SceneNode();
 
+	// This is to release all the DirectX 11
+	// objects of the assets
 	void ReleaseAll();
 
+	// This adds a child node
 	void AddChildNode(SceneNode* n);
 
+	// This removes a child node
 	bool DetachNode(SceneNode* n);
 
+	// This function executes the draw function of the nodes
+	// and its children
 	void Execute(XMMATRIX* world, XMMATRIX* view, XMMATRIX* projection);
 
+	// This updates the position of the node using
+	// the asset's collider's values
 	void UpdateCollisionTree(XMMATRIX* world);
 
+	// This checks collision of one object
+	// against another
 	bool CheckCollision(SceneNode* compare_tree);
 
+	// This checks if an object is interacting
+	// with another
 	bool CheckActionCollision(SceneNode* compare_tree);
 
-	void CalculateSphereCollisionDetails(SceneNode* compare_tree);
-	void CalculateBoxCollisionDetails(SceneNode* compare_tree);
-
+	// This checks if an object is colliding with
+	// something below
 	bool CheckNodeBottomCollision(SceneNode* compare_tree);
 
+	// This calculates collision for spheres
+	void CalculateSphereCollisionDetails(SceneNode* compare_tree);
+
+	// This calculates collision for boxes
+	void CalculateBoxCollisionDetails(SceneNode* compare_tree);
+
+	// These receive data needed for box collision checks
 	void CalculateBoxDimensions1(XMVECTOR v, Asset* obj);
 	void CalculateSphereDimensions1(XMVECTOR v, Asset* obj);
 
+	// These receive data needed for sphere collision checks
 	void CalculateBoxDimensions2(XMVECTOR v, Asset* obj);
 	void CalculateSphereDimensions2(XMVECTOR v, Asset* obj);
 
+	// This applies gravity to objects that
+	// has gravity enabled
 	void ApplyGravity(double deltaTime);
 
+	// This returns position of the collider
 	XMVECTOR GetWorldColliderCentrePos();
+
+	// This returns position of the node
 	XMVECTOR GetWorldPos();
 
+	// These set positions for the node
 	void SetXPos(float x);
 	void SetYPos(float y);
 	void SetZPos(float z);
 
+	// This retricts the pitch of the
+	// node
 	void RestrictPitch();
 
+	// This sets the yaw angle
 	void SetYAngle(float angle);
 
+	// This makes the entity look at
+	// something
 	void LookAt();
+	
+	// This checks if the player is in
+	// range of the entity
 	void CheckInRange(XMVECTOR other_pos);
 
+	// This sets and gets the
+	// current of Z Pos
 	void SetCurZPos();
 	float GetCurZPos();
 
+	// Returns the position of each axis
 	float GetXPos();
 	float GetYPos();
 	float GetZPos();
 
+	// Returns the angle of each axis
 	float GetXAngle();
 	float GetYAngle();
 	float GetZAngle();
 
+	// This returns the scale of each axis
 	float GetXScale();
 	float GetYScale();
 	float GetZScale();
 
+	// This returns the gravity speed
 	float GetGravitySpeed();
 
+	// This restricts the position of the node
+	// if it's colliding
 	void RestrictPos(bool isCollding);
+
+	// This updates the position of the node
+	// if it's not colliding
 	void UpdatePos(bool isColliding);
 
+	// This moves the asset in all directions
 	void MoveAsset(float x_dist, float y_dist, float z_dist);
+
+	// This rotates the asset in all directions
 	void RotateAsset(float pitch_degrees, float yaw_degrees, float roll_degrees);
 
+	// This resets to the asset's initial position
 	void ResetToInitalPos();
 
+	// This returns a random spot
+	// for the entity to move towards
 	float GetRandomSpot();
+
+	// This sets the current spot
+	// to its previous spot
 	void SetToPreviousSpot();
 
+	// Set and Get the colliding state
 	void SetCollideState(bool state);
 	bool IsColliding();
 
+	// Set and Get the interacting state
 	void SetInteractState(bool state);
 	bool IsInteracting();
 
+	// Set and Get the on ground state
 	void SetOnGroundState(bool state);
 	bool GetOnGroundState();
 
+	// Set and Get in Range state
 	void SetInRangeState(bool state);
 	bool GetInRangeState();
 
+	// Set and Get Is Halted state
 	void SetHaltState(bool state);
 	bool isHalted();
 
+	// Returns the size of children
 	int GetChildrenSize();
 
+	// This returns the node's children nodes
 	vector<SceneNode*> GetChildren();
 
+	// Returns the weapon node equipped 
+	// by its parent node
 	SceneNode* GetEquippedWeaponNode();
+
+	// Returns the crate node pushed 
+	// by its parent node
 	SceneNode* GetPushingCrate();
 
 };
