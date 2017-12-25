f0308e4cb313258e6448b54cc90b39bae5cc9e6b
CodeBreaker707 <2solak01@solent.ac.uk>
2017-12-25 12:44:12 +0300

    Commented Statik.h

--- Assignment 2/CGP600-AE2/Statik.h
+++ Assignment 2/CGP600-AE2/Statik.h
@@ -4,10 +4,10 @@
 
 class Statik : public Asset
 {
-private:
 
 public:
 
+	// Constructor and Destructor
 	Statik(ID3D11Device* D3DDevice, ID3D11DeviceContext* ImmediateContext, char* assetFile, char* textureFile, float x_scale, float y_scale, float z_scale);
 	~Statik();
 
